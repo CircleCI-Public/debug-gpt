d1b341bb0eef2cb6d16c963f17d27d4aac335f96
webster <michael@webster.fyi>
2023-08-01 12:20:59 -0400

    move to using chrome debugger api.

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+chrome.action.onClicked.addListener(function (tab) {
+  console.log('Adding listener...');
+  if (tab.url.startsWith('http')) {
+    console.log('Enabling debugger for tab')
+    chrome.debugger.attach({ tabId: tab.id }, '1.2', function () {
+      chrome.debugger.sendCommand(
+        { tabId: tab.id },
+        'Network.enable',
+        {},
+        function () {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          }
+        }
+      );
+    });
+  } else {
+    console.log('Debugger can only be attached to HTTP/HTTPS pages.');
+  }
+});
+
+chrome.debugger.onEvent.addListener(function (source, method, params) {
+  if (method === 'Network.responseReceived') {
+    console.log('Response received:', params.response);
+    // Perform your desired action with the response data
+  } else {
+    console.log('Got some other event');
+  }
+});
