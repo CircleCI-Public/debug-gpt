bd48786c576a725ea0a2da54097b09c968b8e3c2
webster <michael@webster.fyi>
2023-08-01 16:37:27 -0400

    Cleanup.
    Remove the older code using content scripts to inject error handlers.

--- inject.js
+++
@@ -1,6 +0,0 @@
-// Listen for error events in the page and send them to the DevTools panel
-console.log("attaching error listener...")
-window.addEventListener("error", function (event) {
-  console.log("got an error...")
-  chrome.runtime.sendMessage({ type: "error", error: event.error.message });
-});
