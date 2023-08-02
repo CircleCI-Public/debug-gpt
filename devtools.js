5713d5698b96dccbec3e28cb5a7150c8b406dbe7
rsylvian <romain.sylvian@gmail.com>
2023-08-02 16:42:39 -0700

    basic communication demo

--- devtools.js
+++ devtools.js
@@ -1,3 +1,34 @@
-chrome.devtools.panels.create("Error Capture", "", "panel.html", () => {
-  console.log('user switched to this panel');
-});
+chrome.devtools.panels.create(
+  'AI Errors',
+  '',
+  '/panel.html',
+  function (extensionPanel) {
+    var _window // Going to hold the reference to panel.html's `window`
+
+    var data = []
+    var port = chrome.runtime.connect({ name: 'devtools' })
+    port.onMessage.addListener(function (msg) {
+      // Write information to the panel, if exists.
+      // If we don't have a panel reference (yet), queue the data.
+      if (_window) {
+        _window.onData(msg)
+      } else {
+        data.push(msg)
+      }
+    })
+
+    extensionPanel.onShown.addListener(function tmp (panelWindow) {
+      extensionPanel.onShown.removeListener(tmp) // Run once only
+      _window = panelWindow
+
+      // Release queued data
+      var msg
+      while ((msg = data.shift())) _window.onData(msg)
+
+      // setup two way communication with the Service Worker
+      _window.notifySW = function (msg) {
+        port.postMessage(msg)
+      }
+    })
+  }
+)
--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,55 @@
 chrome.action.onClicked.addListener(function (tab) {
-  console.log('Adding listener...');
+  console.log('Adding listener...')
   if (tab.url.startsWith('http')) {
     console.log('Enabling debugger for tab')
     chrome.debugger.attach({ tabId: tab.id }, '1.2', function () {
       chrome.debugger.sendCommand(
         { tabId: tab.id },
-        'Network.enable',
+        'Log.enable',
         {},
         function () {
           if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
+            console.error(chrome.runtime.lastError)
           }
         }
-      );
-    });
+      )
+    })
   } else {
-    console.log('Debugger can only be attached to HTTP/HTTPS pages.');
+    console.log('Debugger can only be attached to HTTP/HTTPS pages.')
   }
-});
+})
+
+var ports = []
+chrome.runtime.onConnect.addListener(function (port) {
+  if (port.name !== 'devtools') return
+  ports.push(port)
+  // Remove port when destroyed (eg when devtools instance is closed)
+  port.onDisconnect.addListener(function () {
+    var i = ports.indexOf(port)
+    if (i !== -1) ports.splice(i, 1)
+  })
+
+  port.onMessage.addListener(function (msg) {
+    // Received message from devtools. Do something:
+    console.log('Received message from devtools page', msg)
+  })
+})
+
+// send message to all DevTools
+const notifyDT = msg => {
+  ports.forEach(function (port) {
+    port.postMessage(msg)
+  })
+}
 
 chrome.debugger.onEvent.addListener(function (source, method, params) {
-  if (method === 'Network.responseReceived') {
-    console.log('Response received:', params.response);
-    // Perform your desired action with the response data
-  } else {
-    console.log('Got some other event', params);
-  }
-});
+  console.log('chrome.debugger.onEvent')
+  notifyDT({
+    time: new Date(),
+    data: {
+      source,
+      method,
+      params
+    }
+  })
+})
