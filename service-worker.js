da677f37f45923215d8ad329cc2438e1492b4e7e
Romain Sylvian <romain.sylvian@gmail.com>
2023-08-07 17:56:44 -0700

    Communication layer between service worker and panel (#2)
    * basic communication demo
    
    * es6 codebase
    
    * onData needs to be in window

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,49 @@
-chrome.action.onClicked.addListener(function (tab) {
-  console.log('Adding listener...');
+chrome.action.onClicked.addListener(tab => {
+  console.log('Adding listener...')
   if (tab.url.startsWith('http')) {
     console.log('Enabling debugger for tab')
-    chrome.debugger.attach({ tabId: tab.id }, '1.2', function () {
-      chrome.debugger.sendCommand(
-        { tabId: tab.id },
-        'Network.enable',
-        {},
-        function () {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError);
-          }
+    chrome.debugger.attach({ tabId: tab.id }, '1.2', () => {
+      chrome.debugger.sendCommand({ tabId: tab.id }, 'Log.enable', {}, () => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError)
         }
-      );
-    });
+      })
+    })
   } else {
-    console.log('Debugger can only be attached to HTTP/HTTPS pages.');
+    console.log('Debugger can only be attached to HTTP/HTTPS pages.')
   }
-});
+})
 
-chrome.debugger.onEvent.addListener(function (source, method, params) {
-  if (method === 'Network.responseReceived') {
-    console.log('Response received:', params.response);
-    // Perform your desired action with the response data
-  } else {
-    console.log('Got some other event', params);
-  }
-});
+let ports = []
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name !== 'devtools') return
+  ports.push(port)
+  // Remove port when destroyed (eg when devtools instance is closed)
+  port.onDisconnect.addListener(() => {
+    const i = ports.indexOf(port)
+    if (i !== -1) ports.splice(i, 1)
+  })
+
+  port.onMessage.addListener(msg => {
+    // Received message from devtools. Do something:
+    console.log('Received message from devtools page', msg)
+  })
+})
+
+// send message to DevTools panel
+const notifyPanel = msg => {
+  ports.forEach(port => {
+    port.postMessage(msg)
+  })
+}
+
+chrome.debugger.onEvent.addListener((source, method, params) => {
+  notifyPanel({
+    time: new Date(),
+    data: {
+      source,
+      method,
+      params
+    }
+  })
+})
