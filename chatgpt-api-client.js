44bff4d8b321d3d215d6ddb2d2d5883172b79da5
rsylvian <romain.sylvian@gmail.com>
2023-08-10 12:50:41 -0700

    fix API call

--- chatgpt-api-client.js
+++ chatgpt-api-client.js
@@ -1,51 +1,49 @@
 class Configuration {
-  constructor(config) {
-    this.apiKey = config.apiKey;
+  constructor (config) {
+    this.apiKey = config.apiKey
   }
 }
 
 class ChatGPTClient {
-  static BASE_URL = "https://api.openai.com/v1";
-  static CHAT_COMPLETIONS = `${ChatGPTClient.BASE_URL}/chat/completions`;
-  static MODEL = "gpt-3.5-turbo";
+  static BASE_URL = 'https://api.openai.com/v1'
+  static CHAT_COMPLETIONS = `${ChatGPTClient.BASE_URL}/chat/completions`
+  static MODEL = 'gpt-3.5-turbo'
 
   static DEBUG_PROMPT = {
-    role: "system",
+    role: 'system',
     content:
-      "You are an expert programmer and debugger. Your job is to accept messages asking for assistance\
-    understanding error messages and return your assessment of the error.",
-  };
-
-  #configuration;
-
-  constructor(configuration) {
-    this.#configuration = configuration;
+      'You are an expert programmer and debugger. Your job is to accept messages asking for assistance understanding error messages and return your assessment of the error.'
   }
 
-  async getErrorCompletion(msg) {
+  #configuration
+
+  constructor (configuration) {
+    this.#configuration = configuration
+  }
+
+  async getErrorCompletion (msg) {
     const response = await fetch(ChatGPTClient.CHAT_COMPLETIONS, {
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${this.#configuration.apiKey}`,
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.#configuration.apiKey}`
       },
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify({
         model: ChatGPTClient.MODEL,
-        message: [
+        messages: [
           ChatGPTClient.DEBUG_PROMPT,
           {
-            role: "user",
-            content: msg,
+            role: 'user',
+            content: msg
           }
         ]
-      }),
-    });
+      })
+    })
 
-    const contents = await response.json();
-    console.log(contents);
-    return contents.choices[0].message.content;
+    const contents = await response.json()
+    console.log(contents)
+    return contents.choices[0].message.content
   }
 }
 
-export { Configuration, ChatGPTClient };
-
+export { Configuration, ChatGPTClient }
